f1374736d899dca756a254f503e75c3454881e6b
Sergey Iryupin <biblelamp@yandex.ru>
2016-12-31 17:05:03 +0300

    EclipseCompositeLaunch version 0.0.5

--- EclipseCompositeLaunch/src/compositelaunch/ui/CompositeTab.java
+++ EclipseCompositeLaunch/src/compositelaunch/ui/CompositeTab.java
@@ -3,8 +3,15 @@ package compositelaunch.ui;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 import compositelaunch.activator.Activator;
@@ -12,19 +19,42 @@ import compositelaunch.activator.Activator;
 /**
  * The CompositeTab class for tab with configuration's list for launch
  *
- * @version 0.0.2 dated Dec 28, 2016
+ * @version 0.0.3 dated Dec 31, 2016
  */
 public class CompositeTab extends AbstractLaunchConfigurationTab {
 
-	public static final String NAME = "Group";
-	private static final String ICON = "icons/launch.png";
+	private static final String NAME_OF_TAB = "Group";
+	private static final String ICON_TAB = "icons/launch.png";
+
+	private Button addConfiguration;
+	private Button deleteConfiguration;
 
 	/*
-	 * Add the desired components in the tab
+	 * Add the desired components on the tab
 	 */
 	@Override
 	public void createControl(Composite parent) {
-		// TODO Auto-generated method stub
+
+		Composite composite = new Group(parent, SWT.BORDER);
+		setControl(composite);
+		composite.setLayout(new GridLayout(2, false));
+
+		GridData gridData = new GridData();
+		gridData.widthHint = 80;
+
+		addConfiguration = new Button(composite, SWT.NONE);
+		addConfiguration.setText("Add...");
+		addConfiguration.setLayoutData(gridData);
+		addConfiguration.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event e) {
+				System.out.println("You push button Add...");
+			}
+		});
+
+		deleteConfiguration = new Button(composite, SWT.NONE);
+		deleteConfiguration.setText("Delete");
+		deleteConfiguration.setLayoutData(gridData);
+
 	}
 
 	@Override
@@ -51,12 +81,13 @@ public class CompositeTab extends AbstractLaunchConfigurationTab {
 	// Name of tab
 	@Override
 	public String getName() {
-		return NAME;
+		return NAME_OF_TAB;
 	}
 
+	// Image for tab
 	@Override
 	public Image getImage() {
-		return AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, ICON).createImage();
+		return AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, ICON_TAB).createImage();
 	}
-	
+
 }
